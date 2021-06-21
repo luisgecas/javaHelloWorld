0001bde78bfee64ef9c270871ebfe71712d07a6b
luisgecas <65181433+luisgecas@users.noreply.github.com>
2021-06-21 16:40:22 -0500

    learning java

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello, World!");
+
+        int numero=12;
+        System.out.println(numero);
+
+    }
+}
